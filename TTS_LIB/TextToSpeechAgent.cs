8262bd2231d77e6149aa1d88f1fde88b118a0650
hesic73 <hesicheng2001@163.com>
2023-03-04 00:41:40 +0800

    Add project files.

---
+++ TTS_LIB/TextToSpeechAgent.cs
@@ -0,0 +1,79 @@
+﻿using Google.Apis.Texttospeech.v1beta1;
+using Google.Apis.Texttospeech.v1beta1.Data;
+using Google.Apis.Services;
+using System.Threading.Tasks;
+
+namespace TTS_LIB
+{
+    public class TextToSpeechAgent
+    {
+        private TexttospeechService _service;
+        // in most cases there is no need to change this
+        // So only change it when necessary instead of passing it
+        // as a parameter every time calling the API
+        private AudioConfig _audioConfig;
+        public AudioConfig AudioConfig
+        {
+            get { return _audioConfig; }
+            set { _audioConfig = value; }
+        }
+
+        private VoiceSelectionParams _voiceSelectionParams;
+        public VoiceSelectionParams VoiceSelectionParams
+        {
+            get { return _voiceSelectionParams; }
+            set { _voiceSelectionParams = value; }
+        }
+
+        public TextToSpeechAgent(string api_key)
+        {
+            _audioConfig = new AudioConfig { AudioEncoding = "Mp3" };
+            _voiceSelectionParams = new VoiceSelectionParams
+            {
+                LanguageCode = "en-US",
+                SsmlGender = "MALE"
+            };
+            _service = new TexttospeechService(new BaseClientService.Initializer
+            {
+                ApplicationName = "Text to Speech",
+                ApiKey = api_key,
+            });
+        }
+
+        // TO DO: more settings
+        public byte[] plainTextToSpeech(string text)
+        {
+            var synthesisInput = new SynthesisInput { Text = text };
+            var ss_request = new SynthesizeSpeechRequest
+            {
+                AudioConfig = _audioConfig,
+                Input = synthesisInput,
+                Voice = _voiceSelectionParams
+            };
+            var request = _service.Text.Synthesize(ss_request);
+            var response = request.Execute();
+
+            return Convert.FromBase64String(response.AudioContent);
+        }
+
+        public Task<byte[]> plainTextToSpeechAsync(string text)
+        {
+            var synthesisInput = new SynthesisInput { Text = text };
+            var ss_request = new SynthesizeSpeechRequest
+            {
+                AudioConfig = _audioConfig,
+                Input = synthesisInput,
+                Voice = _voiceSelectionParams
+            };
+            var request = _service.Text.Synthesize(ss_request);
+
+            return Task<byte[]>.Run(()=>
+            {
+                var response = request.Execute();
+
+                return Convert.FromBase64String(response.AudioContent);
+            });
+
+        }
+    }
+}
---
+++ TTS_LIB/Utils.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+
+namespace TTS_LIB
+{
+    public static class Utils
+    {
+        private const int speech_synthesis_limit = 5000 - 10;
+        public static void WriteToFile(string path, byte[] data)
+        {
+            using (var stream = File.Open(path, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(data);
+                }
+            }
+
+        }
+
+
+        /// <summary>
+        /// split a very long string into smaller strings 
+        /// Had better split by periods
+        /// </summary>
+        /// <param name="long_string"></param>
+        /// <returns></returns>
+        public static IList<string> SplitLongString(string long_string)
+        {
+            var length = long_string.Length;
+            var ls = new List<string>();
+
+            int start = 0;
+            int end = Math.Min(speech_synthesis_limit - 1, length - 1);
+
+            if (length < speech_synthesis_limit)
+            {
+                return new List<string> { long_string };
+            }
+
+            while (true)
+            {
+                var period_index = long_string.LastIndexOf('.', end);
+                if (period_index >= start)
+                {
+                    end = period_index;
+                }
+                ls.Add(long_string.Substring(start, end - start + 1));
+                start = end + 1;
+                end = start + speech_synthesis_limit - 1;
+                if (end >= length)
+                {
+                    ls.Add(long_string.Substring(start));
+                    break;
+                }
+            }
+
+
+            return ls;
+        }
+    }
+}
